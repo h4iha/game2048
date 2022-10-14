ed673c289bdc4c7e72b7e4aab7a8fab044dd3732
h4iha <h4iha2000@gmail.com>
2022-10-14 13:17:11 +0700

    add UI game

---
+++ Assets/Script/GridManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridManager : MonoBehaviour
+{
+    public int cellsInRow;
+    public GridLayoutGroup gridGame;
+
+    void Start()
+    {
+
+    }
+
+
+}
